ab7e0a744ecf5a3f89c2a19a707498dd54e8b7d9
Cool-projects-dev <eliasgamer72@outlook.com>
2025-08-10 17:00:53 +0300

    add version 0.1
    What is new 📌
    1. Added FNAF 1 wiki
    2. Added support for sending sugestions
    3.Made all the parts functional

---
+++ index.js
@@ -0,0 +1,31 @@
+var activePage = "home";
+
+function $(selector) {
+  return document.querySelector(selector);
+}
+
+function hide(id) {
+  $("#" + id).style.display = "none";
+}
+
+function show(id) {
+  $("#" + id).style.display = "block";
+}
+
+function showPage(id) {
+  hide(activePage);
+  $(`#top-menu-bar a[data-page='${activePage}']`).classList.remove("active");
+  show(id);
+  $(`#top-menu-bar a[data-page='${id}']`).classList.add("active");
+  activePage = id;
+}
+
+showPage(activePage);
+
+$("#top-menu-bar").addEventListener("click", function (e) {
+  var id = e.target.dataset.page;
+  console.info("click on menu-bar", id);
+  if (id) {
+    showPage(id);
+  }
+});
